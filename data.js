a4b0bccc3daebb10ab20ef8adc3b84ba1004c7f1
skyblaster89 <skyblaster89@gmail.com>
2021-01-29 12:28:22 -0600

    Construction

---
+++ data.js
@@ -0,0 +1,55 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-construction",
+      "name": "Construction",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "Construction 1",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6280482071685327,
+          "pitch": 0.2328794047601086,
+          "title": "Construction Intro",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Construction",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
